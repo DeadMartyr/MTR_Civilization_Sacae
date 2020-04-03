80400eb62b67122aeda900f3359e847d50d5967a
AngelotheHugMonster <aarriaga1003@gmail.com>
2020-04-03 18:41:03 -0400

    Reworked Temporary Civ UA
    Made placeholder UA, succesffully applied it to the subjects wanted
    --all 3 kinds of cavalry, only to Sacae
    It is *temporary* placeholder to test, currently working on custom effect to input

--- MTR_Civilization_Sacae/Core/MTR_Sacae_Scripts.lua
+++ MTR_Civilization_Sacae/Core/MTR_Sacae_Scripts.lua
@@ -3,6 +3,105 @@
 -- DateCreated: 2020-03-28 12:11:06
 --------------------------------------------------------------
 
+--NOTE
+--It's been a while since I've used lua, i remember that you still declared variables with datatypes(int, String, etc)
+--but the file im referencing doesn't so I'm writing it with the same syntax
+--I've used java for a while and this hurts writing without "(" and ")" and ";"
+
+
+--CONCERNS
+--Are theres variables unique to each unit?
+	--If not would I have to make a table with the unit's unitID and the corresponding variables I need?
+--How do I even query data? I don't actually know what Object/function/anything to call
+
+--------------------------------------------------------------------------
+--UTILITIES
+--------------------------------------------------------------------------
+function MTR_ValidTrait(sTrait)
+		local tValid = {}
+		----------------
+		--Chrisy's Vodoo Magic to get array of all players with Trait
+		----------------
+		return tValid;
+end
+
+function MTR_CalculateDistance(aPosX, aPosY, bPosX, bPosY)
+		-----------------------------------------------------------------
+		--VODOO MAGIC TO GET DISTANCE AND RETURN IT AS INTEGER
+		-----------------------------------------------------------------------
+end
+
+function MTR_ApplyHeal(newDistance, oldDistance)
+		------------------------------------------------------------
+		--Heal if you moved extra distance away, 
+		--making sure to not heal for distance that was already applied before
+		--
+		--Assumes newDistance > oldDistance
+		-------------------------------------------------------------
+end
+
+--------------------------------------------------------------------------
+--Constants
+--------------------------------------------------------------------------
+
+	--Dunno if I need this, but I don't know how else the game is gonna know what units this'll apply to
+	local sTrait = "TRAIT_CIVILIZATION_MTR_SACAE_UA";
+	local tTraitPlayers = MTR_ValidTrait(sTrait);
+
+
+    --At start of turn this will be set, does not need to be defined here, just ensure that at turn start it sets this to whereever the unit is
+	local startPosX;
+	local startPosY;
+	
+	--At start resets to zero, upon the unit moving another function calculates distance the unit is from where it started that turn, 
+	--if it's bigger than this, it'll set this to what distance it has and heal accordingly.
+	--i.e. if i move two spaces away heal me 10, if i move 1 space back to where i started, 
+	--i dont heal, if i move 2 more tiles away then im 3 tiles away and heal me an additional 5
+	local maxDistanceCoveredThisTurn;
+	
+	local healPerSpaceBase = 5;
+	local healScaling = 1.3; --Scaling with distance or diminishing returns can be changed here
+
+
+--------------------------------------------------------------------------
+--Runs at turn start
+--------------------------------------------------------------------------
+function MTR_SacaeUA_TurnStart()
+	--Record Position at start of turn
+	startPosX = --GetUnitPosX();
+	startPosY = --GetUnitPosY();
+	--Reset distance to zero
+	maxDistanceCoveredThisTurn = 0;
+end
+
+--------------------------------------------------------------------------
+--Runs when player unit moves
+--------------------------------------------------------------------------
+function MTR_SacaeUA_UnitMove()
+	local currentPosX = --GetUnitPosX();
+	local currentPosY = --GetUnitPosY();
+
+	local currentDistanceCovered = MTR_CalculateDistance(startPosX, startPosY, currentPosX, currentPosY);
+
+	if (currentDistanceCovered > maxDistanceCoveredThisTurn)
+		MTR_ApplyHeal(currentDistanceCovered, maxDistanceCovered);
+		maxDistanceCovered = currentDistanceCovered;
+	end
+	else
+		--DO NOTHING;
+	end
+end
+
+-------------------------------------------------
+--Inputting Functions into the Game Events (This is what I think this does)
+-------------------------------------------------
+Events.TurnBegin.Add(MTR_SacaeUA_TurnStart());
+Events.UnitMoved.Add(MTR_SacaeUA_UnitMove());
+
+
+
+
+
 --------------------------------------------------------------
 --UNIT HEALS BY MOVING
 --
@@ -49,4 +148,3 @@
 --
 -- GOING WITH IDEA 3
 --------------------------------------------------------------------------
-
