6f667908bb4fbfddab9bf66e6e8cb2d2e51e6437
AngelotheHugMonster <aarriaga1003@gmail.com>
2020-04-06 18:06:43 -0400

    Sacae Civ Ability Implemented
    Sacae's Ability "Call Of The Wind" Implemented:
    Heals Cavalry(Light, Heavy, Ranged) Based on distance traveled from starting position (That start of turn), does not apply if you travel equal distance from the origin or if you make back to it (Have to go further)

--- MTR_Civilization_Sacae/Core/MTR_Sacae_Scripts.lua
+++ MTR_Civilization_Sacae/Core/MTR_Sacae_Scripts.lua
@@ -1,150 +1,288 @@
 -- MTR_Sacae_Scripts
--- Author: Angelo
+-- Author: Martyr
 -- DateCreated: 2020-03-28 12:11:06
 --------------------------------------------------------------
 
---NOTE
---It's been a while since I've used lua, i remember that you still declared variables with datatypes(int, String, etc)
---but the file im referencing doesn't so I'm writing it with the same syntax
---I've used java for a while and this hurts writing without "(" and ")" and ";"
+--At turn start:
+	--Reset information on max distances to zero
+	--Record positions of all units for the player whose turn it is
+--Upon Unit Moving:
+	--Calculate Distance from starting position
+		--IF greater than previous maxdistance for that unitID then 
+			--HEAL
+			--Record maxdistance
+		--ELSE DO NOTHING
 
-
---CONCERNS
---Are theres variables unique to each unit?
-	--If not would I have to make a table with the unit's unitID and the corresponding variables I need?
---How do I even query data? I don't actually know what Object/function/anything to call
-
---------------------------------------------------------------------------
+--====================================================================
 --UTILITIES
---------------------------------------------------------------------------
-function MTR_ValidTrait(sTrait)
-		local tValid = {}
-		----------------
-		--Chrisy's Vodoo Magic to get array of all players with Trait
-		----------------
-		return tValid;
+--Credit: Chrisy15 and LeeS
+--====================================================================
+--------
+--Created by Chrisy15, didn't retool as it functions exactly the way I need it
+--------
+function MTR_getValidPlayersWithTrait(sTrait)
+	local tValidPlayers = {}
+	
+	for k, v in ipairs(PlayerManager.GetWasEverAliveIDs()) do
+		local leaderType = PlayerConfigurations[v]:GetLeaderTypeName()
+        for trait in GameInfo.LeaderTraits() do
+            if trait.LeaderType == leaderType and trait.TraitType == sTrait then 
+                tValidPlayers[v] = true 
+            end;
+        end
+        if not tValidPlayers[v] then
+            local civType = PlayerConfigurations[v]:GetCivilizationTypeName()
+            for trait in GameInfo.CivilizationTraits() do
+                if trait.CivilizationType == civType and trait.TraitType == sTrait then 
+                    tValidPlayers[v] = true 
+                end;
+            end
+        end
+    end
+    return tValidPlayers
 end
 
-function MTR_CalculateDistance(aPosX, aPosY, bPosX, bPosY)
-		-----------------------------------------------------------------
-		--VODOO MAGIC TO GET DISTANCE AND RETURN IT AS INTEGER
-		-----------------------------------------------------------------------
+
+
+--------
+--Created by LeeS, didn't retool as it functions exactly the way I need it
+--------
+function UnitIsCombatUnit(pUnit, bConsiderReligious)
+	if (pUnit ~= nil) then
+		local tUnitData = GameInfo.Units[pUnit:GetType()]
+		if (tUnitData ~= nil) then
+			if (tUnitData.Combat > 0) or (tUnitData.RangedCombat > 0) or (tUnitData.Bombard > 0) then
+				return true
+			elseif (bConsiderReligious == true) then
+				if (tUnitData.ReligiousStrength > 0) and ((tUnitData.PromotionClass == "PROMOTION_CLASS_APOSTLE") or (tUnitData.PromotionClass == "PROMOTION_CLASS_INQUISITOR")) then
+					return true
+				end
+			end
+		end
+	end
+	return false
 end
 
-function MTR_ApplyHeal(newDistance, oldDistance)
-		------------------------------------------------------------
-		--Heal if you moved extra distance away, 
-		--making sure to not heal for distance that was already applied before
-		--
-		--Assumes newDistance > oldDistance
-		-------------------------------------------------------------
+----------------------------------------------
+--My custom method that generates a table of Unit Types with tags for the Classes it's meant to affect
+----------------------------------------------
+function MTR_getValidUnits()
+	local tTable = {}
+	print("SELECT Type, Tag FROM TypeTags WHERE (Type NOT NULL OR Tag NOT NULL) AND Tag = 'CLASS_LIGHT_CAVALRY'");
+	local tQuery = DB.Query("SELECT Type, Tag FROM TypeTags WHERE (Type NOT NULL OR Tag NOT NULL) AND Tag = 'CLASS_LIGHT_CAVALRY'");
+	print("Query Results");
+	print(tQuery);
+	for k, v in ipairs(tQuery) do
+		tTable[v.Type] = true;
+		print("ValidUnit:");
+		print(v.Type);
+	end
+	
+	print("SELECT Type, Tag FROM TypeTags WHERE (Type NOT NULL OR Tag NOT NULL) AND Tag = 'CLASS_HEAVY_CAVALRY'");
+	tQuery = DB.Query("SELECT Type, Tag FROM TypeTags WHERE (Type NOT NULL OR Tag NOT NULL) AND Tag = 'CLASS_HEAVY_CAVALRY'");
+	print("Query Results");
+	print(tQuery);
+	for k, v in ipairs(tQuery) do
+		tTable[v.Type] = true;
+		print("ValidUnit:");
+		print(v.Type);
+	end
+	
+	print("SELECT Type, Tag FROM TypeTags WHERE (Type NOT NULL OR Tag NOT NULL) AND Tag = 'CLASS_RANGED_CAVALRY'");
+	tQuery = DB.Query("SELECT Type, Tag FROM TypeTags WHERE (Type NOT NULL OR Tag NOT NULL) AND Tag = 'CLASS_RANGED_CAVALRY'");
+	print("Query Results");
+	print(tQuery);
+	for k, v in ipairs(tQuery) do
+		tTable[v.Type] = true;
+		print("ValidUnit:");
+		print(v.Type);
+	end
+	return tTable;
 end
+----------------------------------------------
 
---------------------------------------------------------------------------
+--====================================================================
+
+
+
+--====================================================================
 --Constants
---------------------------------------------------------------------------
+--====================================================================
 
-	--Dunno if I need this, but I don't know how else the game is gonna know what units this'll apply to
 	local sTrait = "TRAIT_CIVILIZATION_MTR_SACAE_UA";
-	local tTraitPlayers = MTR_ValidTrait(sTrait);
-
-
-    --At start of turn this will be set, does not need to be defined here, just ensure that at turn start it sets this to whereever the unit is
-	local startPosX;
-	local startPosY;
+	local tValidPlayerList = MTR_getValidPlayersWithTrait(sTrait); --Key is PlayerID, returns true or nil based on what MTR_getValidPlayersWithTrait returns
+	local tValidUnitList = MTR_getValidUnits(); --Table containing all UnitTypes that are Cavalry
 	
-	--At start resets to zero, upon the unit moving another function calculates distance the unit is from where it started that turn, 
-	--if it's bigger than this, it'll set this to what distance it has and heal accordingly.
-	--i.e. if i move two spaces away heal me 10, if i move 1 space back to where i started, 
-	--i dont heal, if i move 2 more tiles away then im 3 tiles away and heal me an additional 5
-	local maxDistanceCoveredThisTurn;
+	local tUnitStartPoses = {}; -- [player][unit] -> {X, Y}
+	local iUnitMaxDistancesCovered = {}; --array of array of integers [player][unit] -> int 
 	
-	local healPerSpaceBase = 5;
-	local healScaling = 1.3; --Scaling with distance or diminishing returns can be changed here
+	local iHealPerSpaceBase = 10;
+	--local iHealScaling = 1.3; --Scaling with distance or diminishing returns can be changed here IMPLEMENT LATER
 
 
---------------------------------------------------------------------------
+
+--====================================================================
+-- Custom Functions
+--====================================================================
+
+
+
+function MTR_CalculateTraveledDistance(iPlayerID, iUnitID, iX, iY) --Calculates traveled distance from starting position
+		print("MTR_CalculateTraveledDistance")
+		local aX = tUnitStartPoses[iPlayerID][iUnitID].X; 
+		local aY = tUnitStartPoses[iPlayerID][iUnitID].Y;
+		
+		local iDistance = Map.GetPlotDistance(iX, iY, aX, aY)--math.sqrt((aX-iX)^2-(aY-iY)^2);
+		print("Distance: ");
+		print(iDistance);
+		return iDistance;
+end
+
+
+function MTR_ApplyHeal(iPlayerID, iUnitID, iNewDistance, iOldDistance)
+		print("MTR_ApplyHeal")
+		local iNewHeal = iHealPerSpaceBase*iNewDistance;
+		local iOldHeal = iHealPerSpaceBase*iOldDistance;
+		print("UnitID: ");
+		print(iUnitID);
+		print("newHeal: ");
+		print(iNewHeal);
+		print("oldHeal: ");
+		print(iOldHeal);
+		
+		local pUnit = Players[iPlayerID]:GetUnits():FindID(iUnitID);
+		pUnit:ChangeDamage(-1*(iNewHeal-iOldHeal)); --negative damage is healing, positive is damage, set to positive right now for testing purposes
+		print("Heal Applied");
+		iUnitMaxDistancesCovered[iPlayerID][iUnitID] = iNewDistance;--records maxdistance
+		print("Set unit Max Distance: ");
+		print(iUnitMaxDistancesCovered[iPlayerID][iUnitID]);
+end
+
+
+--------
+--Based off LeeS' work
+--------
+function MTR_ResetDistances(iPlayerID)
+	print("MTR_ResetDistances")
+	if tValidPlayerList[iPlayerID] then
+		iUnitMaxDistancesCovered[iPlayerID] = {}
+		print("PlayerID: ");
+		print(iPlayerID);
+		local pPlayer = Players[iPlayerID]
+		for i,pUnit in pPlayer:GetUnits():Members() do
+			print("UnitID: ");
+			print(pUnit:GetID());
+			iUnitMaxDistancesCovered[iPlayerID][pUnit:GetID()] = 0;
+			print("Distance Reset to 0");
+			print(iUnitMaxDistancesCovered[iPlayerID][pUnit:GetID()]);
+		end
+	end
+end
+function MTR_ResetStartPoses(iPlayerID)
+	print("MTR_ResetStartPoses")
+	if tValidPlayerList[iPlayerID] then
+		tUnitStartPoses[iPlayerID] = {}
+		print("PlayerID: ");
+		print(iPlayerID);
+		local pPlayer = Players[iPlayerID]
+		for i,pUnit in pPlayer:GetUnits():Members() do
+			print("UnitID: ");
+			print(pUnit:GetID());
+			tUnitStartPoses[iPlayerID][pUnit:GetID()] = {X = 0, Y = 0};
+			print("Pos Reset to 0,0")
+		end
+	end
+end
+
+--------
+--Retooled from LeeS, changed to return a table instead of directly setting the values on an existing table
+--Can be used like this at multiple stages now, and doesn't need clutter from "bIsFirstTimeThisTurn"
+--------
+function MTR_GetUnitPositions(iPlayerID)
+	print("MTR_GetUnitPositions")
+	local tUnitPoses = {}
+
+	if tValidPlayerList[iPlayerID] then
+		tUnitPoses[iPlayerID] = {}
+		print("PlayerID: ");
+		print(iPlayerID);
+		local pPlayer = Players[iPlayerID]
+		for i,pUnit in pPlayer:GetUnits():Members() do
+			print("UnitID: ");
+			print(pUnit:GetID());
+			tUnitPoses[iPlayerID][pUnit:GetID()] = {X = pUnit:GetX(), Y = pUnit:GetY()}
+			print("Unit X: ");
+			print(pUnit:GetX());
+			print("Unit Y: ");
+			print(pUnit:GetY());
+		end
+	end
+	return tUnitPoses[iPlayerID]--Returns the table for that playerID
+end
+
+
+
+
+--====================================================================
 --Runs at turn start
---------------------------------------------------------------------------
-function MTR_SacaeUA_TurnStart()
-	--Record Position at start of turn
-	startPosX = --GetUnitPosX();
-	startPosY = --GetUnitPosY();
-	--Reset distance to zero
-	maxDistanceCoveredThisTurn = 0;
+--Essentially just calls the Utilities as long as it's not called more than once
+--====================================================================
+function MTR_PlayerTurnActivated(iPlayerID, bIsFirstTimeThisTurn)
+	print("Player Turn Activated");
+	if (tValidPlayerList[iPlayerID] ~= true) then return end --abort if not ValidPlayer
+	print("Is ValidPlayer")
+	if (bIsFirstTimeThisTurn == false) then return end --abort if not first time this turn
+	print("Is FirstTime")
+	
+	
+	MTR_ResetStartPoses(iPlayerID);--resets values for this player to zero
+	MTR_ResetDistances(iPlayerID);--resets values for this player to zero
+	tUnitStartPoses[iPlayerID] = MTR_GetUnitPositions(iPlayerID);--SetPlayerPositions (specific for that playerID)
 end
 
---------------------------------------------------------------------------
+--====================================================================
 --Runs when player unit moves
---------------------------------------------------------------------------
-function MTR_SacaeUA_UnitMove()
-	local currentPosX = --GetUnitPosX();
-	local currentPosY = --GetUnitPosY();
-
-	local currentDistanceCovered = MTR_CalculateDistance(startPosX, startPosY, currentPosX, currentPosY);
-
-	if (currentDistanceCovered > maxDistanceCoveredThisTurn)
-		MTR_ApplyHeal(currentDistanceCovered, maxDistanceCovered);
-		maxDistanceCovered = currentDistanceCovered;
+--====================================================================
+function MTR_SacaeUA_UnitMoved(iPlayerID, iUnitID, iX, iY, locallyVisible, stateChange)
+	print("Unit Moved Proc:");
+	if (tValidPlayerList[iPlayerID] ~= true) then return end --abort if not ValidPlayer
+	print("Unit is from ValidPlayer");
+	local pPlayer = Players[iPlayerID];
+	local pUnit = pPlayer:GetUnits():FindID(iUnitID);
+	local sType = GameInfo.Units[pUnit:GetType()].UnitType
+	print("UnitType: ");
+	print(sType);
+	print("UnitId: ");
+	print(iUnitID);
+	if (tValidUnitList[sType] ~= true) then return end --abort if not valid UnitType
+	print("Valid UnitType");
+	
+	
+	local iNewDistance = MTR_CalculateTraveledDistance(iPlayerID, iUnitID, iX, iY);
+	
+	print("iUnitMaxDistancesCovered[iPlayerID][UnitID] :")
+	print(iUnitMaxDistancesCovered[iPlayerID][iUnitID]);
+	if (iUnitMaxDistancesCovered[iPlayerID][iUnitID] == nil) then --Safety if value doesn't exist
+		iUnitMaxDistancesCovered[iPlayerID][iUnitID] = 0;
+		print("iOldDistance set to 0 because iUnitMaxDistancesCovered[iPlayerID][pUnit:GetID()] was nil");
 	end
-	else
-		--DO NOTHING;
+	iOldDistance = iUnitMaxDistancesCovered[iPlayerID][iUnitID];
+	
+	print("NewDist: "); 
+	print(iNewDistance);
+	print("OldDist: ");
+	print(iOldDistance);
+	if (iNewDistance > iOldDistance) then --getting error for comparing number to nil here
+		print("NewDist is greater than OldDist!")
+		MTR_ApplyHeal(iPlayerID, iUnitID, iNewDistance, iOldDistance);
+		print("Double Checking Max Distance: ");
+		print(iUnitMaxDistancesCovered[iPlayerID][iUnitID]);
 	end
+	--else do nothing
 end
 
--------------------------------------------------
+--====================================================================
 --Inputting Functions into the Game Events (This is what I think this does)
--------------------------------------------------
-Events.TurnBegin.Add(MTR_SacaeUA_TurnStart());
-Events.UnitMoved.Add(MTR_SacaeUA_UnitMove());
-
-
-
-
-
---------------------------------------------------------------
---UNIT HEALS BY MOVING
---
---IDEAS:
---  1) As soon as move, heal based on moves used
---	ref: 
---  Lua objects: GetMaxMoves and GetExtraMoves
---  Events: UnitMovementPointsChanged /UnitMovementPointsCleared (not this one)
---		Pros:
---			+Directly visible and apparent, flashy
---			+Easy to implement
---		Cons:
---			-Would have to be insignificant amount or else people would move to heal and come back to same spot and then attack, it'd be really annoying to fight
---
---
---	2) At turn end, heal based on moves used
---	ref: 
---  Lua objects: GetMaxMoves and GetExtraMoves
---  Events: TurnEnd
---		Pros: 
---			+Easy to implement
---			+Could be up-d as it would be less cheesy to happen at the end of the turn
---		Cons:
---			-Loses flashiness, like you might as well just give them the march promotion
---			-**MAJOR**, if attacking reduces units moves to 0, this basically just gives all calvary a heal no matter what action they take, could be stationary and shoot and get the heal
---			-Still cheesy as people could move to use the moves and then just go back to same spot
---
---
---	3) At turn end, heal based on distance covered
---	ref: 
---  Lua objects: GetLocation
---  Events: TurnEnd TurnBegin // UnitMoved, UnitMoveComplete, UnitMovementPointsChanged
---		Pros:
---			+Best reflects what I'm trying to do
---			+Woods and hills wood restrict movement and therefore the healing directly, gives a clear weakness, and because of this can be more impactful on flat terrain
---			+Prevents cheese with moving out and into the same tile to get the heal
---		Cons:
---			-Most difficult to implement
---				Need to figure out how to get starting location at start of turn and then ending location at end of turn and apply heal based on the distance between the two
---			-Cannot be set to whenever you move, otherwise the cheese with "move out two spaces, move back two spaces, get heal" is still an issue
---				+POTENTIALLY, can make it so if it remembers where you started at the start of the turn and if you move X away from it, then it heals you for that amount, but not anymore if you move back closer to it, only if you go farther
---			-Because of above, it loses a bit of flashiness UNLESS that idea can be ironed out, then it reflects the idea even better than idea number 1
---
---
--- GOING WITH IDEA 3
---------------------------------------------------------------------------
+--====================================================================
+Events.PlayerTurnActivated.Add(MTR_PlayerTurnActivated);
+Events.UnitMoved.Add(MTR_SacaeUA_UnitMoved);
